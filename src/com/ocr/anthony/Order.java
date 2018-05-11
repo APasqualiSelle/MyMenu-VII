a935436fc7f9c4788c0420a21df339c1569900b7
APasqualiSelle <33148018+APasqualiSelle@users.noreply.github.com>
2018-05-11 22:28:59 +0200

    Given return int values to the methods askSomething() and askMenu(); added new attribut 'String orderSummary' used in askSomething() and runMenus();

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Order {
     Scanner sc = new Scanner(System.in);
+    public String orderSummary = "";
 
     /**
      * Display all available menus in the restaurant.
@@ -15,8 +16,10 @@ public class Order {
         System.out.println("3 - végétarien");
         System.out.println("Que souhaitez-vous comme menu ?");
     }
+
     /**
      * Display a selected menu.
+     *
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
@@ -35,29 +38,28 @@ public class Order {
                 break;
         }
     }
+
     /**
      * Run asking process for a menu.
      */
     public void runMenu() {
-        this.displayAvailableMenu();
-        int nbMenu;
-        do {
-            nbMenu = sc.nextInt();
-            this.displaySelectedMenu(nbMenu);
-            switch (nbMenu) {
-                case 1:
-                    askSide(true);
-                    askDrink();
-                    break;
-                case 2:
-                    askSide(true);
-                    break;
-                case 3:
-                    askSide(false);
-                    askDrink();
-                    break;
-            }
-        } while (nbMenu < 1 || nbMenu > 3);
+
+        int nbMenu = askMenu();
+
+        switch (nbMenu) {
+            case 1:
+                askSide(true);
+                askDrink();
+                break;
+            case 2:
+                askSide(true);
+                break;
+            case 3:
+                askSide(false);
+                askDrink();
+                break;
+        }
+
     }
 
     /**
@@ -66,16 +68,22 @@ public class Order {
     public void runMenus() {
         System.out.println("Combien de menus souhaitez-vous commander ?");
         int menuQuantity = sc.nextInt();
+        orderSummary = "Résumé de votre commande :%n";
         for (int i = 0; i < menuQuantity; i++) {
+            orderSummary += "Menu " + (i + 1) + ":%n";
             runMenu();
         }
+        System.out.println("");
+        System.out.println(String.format(orderSummary));
     }
+
     /**
      * Display a selected side depending on all sides enable or not.
      * All sides = vegetables, frites and rice
      * No all sides = rice or not
-     * @param nbSide The selected Side
-     * @param allSidesEnable  enable display for all side or not
+     *
+     * @param nbSide         The selected Side
+     * @param allSidesEnable enable display for all side or not
      */
     public void displaySelectedSide(int nbSide, boolean allSidesEnable) {
         if (allSidesEnable) {
@@ -107,8 +115,10 @@ public class Order {
             }
         }
     }
+
     /**
      * Display a selected drink.
+     *
      * @param nbDrink The selected drink.
      */
     public void displaySelectedDrink(int nbDrink) {
@@ -127,10 +137,12 @@ public class Order {
                 break;
         }
     }
+
     /**
      * Display all available sides depending on all sides enable or not.
      * All sides = vegetables, frites and rice
      * No all sides = rice or not
+     *
      * @param allSideEnable enable display for all side or not
      */
     public void displayAvailableSide(boolean allSideEnable) {
@@ -159,10 +171,12 @@ public class Order {
 
     /**
      * Display a question about a category in the standard input, get response and display it
-     * @param category the category of the question
+     *
+     * @param category  the category of the question
      * @param responses available responses
+     * @return the number of the selected choice
      */
-    public void askSomething(String category, String[] responses) {
+    public int askSomething(String category, String[] responses) {
         System.out.println("Choix " + category);
         for (int i = 1; i <= responses.length; i++)
             System.out.println(i + " - " + responses[i - 1]);
@@ -172,24 +186,31 @@ public class Order {
         do {
             nbResponse = sc.nextInt();
             responseIsGood = (nbResponse >= 1 && nbResponse <= responses.length);
-            if (responseIsGood)
-                System.out.println("Vous avez choisi comme " + category + " : " + responses[nbResponse - 1]);
-            else {
+            if (responseIsGood) {
+                String choice = "Vous avez choisi comme " + category + " : " + responses[nbResponse - 1];
+                orderSummary += choice + "%n";
+                System.out.println(choice);
+            } else {
                 boolean isVowel = "aeiouy".contains(Character.toString(category.charAt(0)));
                 if (isVowel)
                     System.out.println("Vous n'avez pas choisi d'" + category + " parmi les choix proposés");
                 else
                     System.out.println("Vous n'avez pas choisi de " + category + " parmi les choix proposés");
             }
+
         } while (!responseIsGood);
+        return nbResponse;
     }
 
     /**
      * Display a question about menu in the standard input, get response and display it
+     *
+     * @return the number of the selected menu
      */
-    public void askMenu() {
+    public int askMenu() {
         String[] menus = {"poulet", "boeuf", "végétarien"};
-        askSomething("menu", menus);
+        int nbMenu = askSomething("menu", menus);
+        return nbMenu;
     }
 
     /**
